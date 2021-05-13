e74e71a82d7705a6fa3fd1c4481502d9c691bcc9
Chase Mitchell <cmitchell1313@gmail.com>
2021-05-13 13:25:21 -0700

    added teleportation system reticle and effects

---
+++ Assets/Scripts/ContinuousMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinuousMovement : MonoBehaviour
+{
+    public XRNode inputSource; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
