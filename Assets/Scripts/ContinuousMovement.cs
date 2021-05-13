cd7502c00a59c650242022f82c359cffbf2a393b
Chase Mitchell <cmitchell1313@gmail.com>
2021-05-13 14:05:37 -0700

    added gravity behavior

--- Assets/Scripts/ContinuousMovement.cs
+++ Assets/Scripts/ContinuousMovement.cs
@@ -6,15 +6,57 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ContinuousMovement : MonoBehaviour
 {
-    public XRNode inputSource; 
+    public XRNode inputSource;
+    public LayerMask groundLayer;
+
+    [SerializeField]
+    private float gravity = -9.8f;
+    private float fallingSpeed;
+
+    private Vector2 inputAxis;
+    private XRRig rig;  
+    private CharacterController character;
+    [SerializeField]
+    private float speed = 1f;
 
     void Start()
     {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+    private void FixedUpdate()
+    {
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+        character.Move(direction * speed * Time.deltaTime);
+
+        bool isGrounded = CheckIfGrounded();
+        if (isGrounded)
+        {
+            fallingSpeed = 0;
+        }
+        else
+        {
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+        }
         
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+
+    bool CheckIfGrounded()
+    {
+        //indicates we are on the ground
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
     }
 
     void Update()
     {
-        
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
     }
+
+    
 }
