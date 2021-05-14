c8e2aa8c872e64e83e57354aa915b0fe1d51bb01
Chase Mitchell <cmitchell1313@gmail.com>
2021-05-14 12:54:53 -0700

    disabled teleport and shoot overlap behavior

--- Assets/TeleportationController.cs
+++ Assets/TeleportationController.cs
@@ -9,12 +9,13 @@ public class TeleportationController : MonoBehaviour
     public InputHelpers.Button teleportActivationButton;
     public float activationThreshold = 0.1f;
 
+    public bool EnableRightTeleport { get; set; } = true;
 
     void Update()
     {
         if (rightTeleportRay)
         {
-            rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+            rightTeleportRay.gameObject.SetActive(EnableRightTeleport && CheckIfActivated(rightTeleportRay));
         }
     }
     public bool CheckIfActivated(XRController controller)
